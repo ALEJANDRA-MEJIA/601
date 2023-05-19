e596587c99cb1171abb40885f137d45612cd02ff
ALEJANDRA <yoselinmejialopez5@gmail.com>
2023-05-19 11:20:42 -0600

    Agregamos funcionMultiplo

---
+++ V4TRIFIV/src/Funciones/funcionMultiplo.java
@@ -0,0 +1,17 @@
+package Funciones;
+
+public class funcionMultiplo {
+    public String retornaMultiplo(int num){
+    if(num % 3==0 && num % 5 == 0){
+    return("TRIFIV");
+    }else if ( num % 3 ==0){
+    return("TRI");
+    }else if ( num % 5 ==0){
+    return("FIV");
+    
+    
+    }
+        return "No es multiplo de ninguno";
+    }
+}
+    
